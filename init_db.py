b1b02a4e085afcf236ed2238c1ddadcd33c124c0
tniu <tniu@infomedia.com.au>
2021-09-11 09:19:23 +1000

    init

--- init_db.py
+++ init_db.py
@@ -4,6 +4,6 @@ conn = sqlite3.connect('database.db')
 print("Opened database successfully");
 
 # conn.execute('CREATE TABLE users (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, addr TEXT, city TEXT, pin TEXT)')
-conn.execute('CREATE TABLE users (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, addr TEXT)')
+conn.execute('CREATE TABLE users (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, grade INTEGER)')
 print("Table created successfully");
 conn.close()
--- server.py
+++ server.py
@@ -15,34 +15,22 @@ def home():
 def new_user():
    return render_template('user.html')
 
-@app.route('/add1',methods = ['POST'])
-def add1():
-  content = request.json
-  print(content)
-  return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-
 @app.route('/add',methods = ['POST'])
 @cross_origin()
 def add():
    if request.method == 'POST':
       try:
-        # nm = request.form['nm']
-        # addr = request.form['add']
-        # city = request.form['city']
-        # pin = request.form['pin']
         content = request.json
-        nm = content['name']
-        # addr = content["address"]
-        addr = 'test'
+        name = content['name']
+        grade = content['grade']
 
-        print(nm)
-        print(addr)
+        print(name)
+        print(grade)
 
         with sql.connect("database.db") as con:
           cur = con.cursor()
 
-          # cur.execute("INSERT INTO users (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
-          cur.execute("INSERT INTO users (name,addr) VALUES (?,?)",(nm,addr) )
+          cur.execute("INSERT INTO users (name,grade) VALUES (?,?)",(name,grade) )
 
           con.commit()
           msg = "Record successfully added"
@@ -77,15 +65,8 @@ def list():
         d = collections.OrderedDict()
         d['id'] = row[0]
         d['name'] = row[1]
-        # d['lastName'] = row[2]
-        # d['Street'] = row[3]
+        d['grade'] = row[2]
         objects_list.append(d)
-      j = json.dumps(objects_list)
-      print(j)
-      # with open('student_objects.js', 'w') as f:
-      #   f.write(j)
-
-
 
   except:
     con.close()
